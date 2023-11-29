958f90b0034c54abade808ffa77667df7318ef44
Marvin Stier <email@marvinstier.de>
2023-11-29 11:42:54 +0100

    tree

--- python-conv/converter.py
+++ python-conv/converter.py
@@ -14,4 +14,16 @@ with open("alphabet.txt", "w") as out_file:
                 out_file.write("\\n")
         out_file.write("\",\n")
 
+with open("tree.txt", "w") as out_file:
+    image_path = "../resources/pixel-art/tree.png"
+    image = Image.open(image_path)
+    width, height = image.size
+    out_file.write("\"")
+    for y in range(height):
+        for x in range(width):
+            r, g, b = image.getpixel((x, y))
+            out_file.write("1" if r+g+b == 0 else "0")
+        if y != height - 1:
+            out_file.write("\\n")
+
 image.close()
